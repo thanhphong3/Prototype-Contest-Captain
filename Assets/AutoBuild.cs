c873ce9e9e8a5965c73fa659e3e2cdf262d8ad97
thanhphong3 <dcvhjb11223344@gamil.com>
2023-02-10 10:10:18 +0700

    Remove old file

---
+++ Assets/AutoBuild.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoBuild : MonoBehaviour
+{
+    public static void ShowCommand()
+    {
+        Debug.Log("Build Done Phong");
+    }
+
+}
