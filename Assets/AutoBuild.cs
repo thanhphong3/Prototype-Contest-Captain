7036c126ea626ee1bf308a92bf9ce7a481d7c4f8
thanhphong3 <dcvhjb11223344@gamil.com>
2023-02-10 12:41:29 +0700

    Update AutoBuild.cs

--- Assets/AutoBuild.cs
+++ Assets/AutoBuild.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class AutoBuild : MonoBehaviour
 {
     public static void ShowCommand()
     {
         Debug.Log("Build Done Phong");
+    }
+    public static void PerformSwitchAndroid()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
     }
 
 }
